3da0f0ecdd696f133781c449f3da14473966bf88
Sergii Pylypenko <x.pelya.x@gmail.com>
2015-08-03 20:51:10 +0300

    Added multiarch support to AndroidBuild.sh, different key theme

--- Android/vice/AndroidBuild.sh
+++ Android/vice/AndroidBuild.sh
@@ -4,14 +4,15 @@
 LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
-ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi-v7a/libSDL.so
+ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/$1/libSDL.so
 
 if [ \! -f vice/configure ] ; then
 	sh -c "cd vice && ./autogen.sh"
 fi
 
-if [ \! -f vice/Makefile ] ; then
-	../setEnvironment-armeabi-v7a.sh sh -c "cd vice && ./configure --host=arm-linux-androideabi --without-x --enable-sdlui --with-sdlsound"
+if [ \! -f vice/$1/Makefile ] ; then
+	mkdir -p vice/$1
+	env PATH=`cd .. ; pwd`:$PATH ../setEnvironment-armeabi-v7a.sh sh -c "cd vice/$1 && ../configure --host=arm-linux-androideabi --without-x --enable-sdlui --with-sdlsound"
 fi
 
-make -C vice && mv -f vice/src/x64 libapplication-armeabi-v7a.so
+make -j4 -C vice/$1 && mv -f vice/$1/src/x64 libapplication-$1.so
